d89333855f78157794e9e83579e06bf0854628d3
shovha_cubex <shovha.saha@cubex.tech>
2017-03-09 14:37:11 +0600

    excel package added

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,35 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index');
+
+Route::get('/excel',function(){
+
+	$arr = [
+			['name'=>'a',
+			'roll'=>1],
+			['name'=>'b',
+			'roll'=>2]
+		];
+
+	Excel::create('Filename', function($excel) use($arr) {
+
+    // Set the title
+    $excel->setTitle('Our new awesome title');
+
+    // Chain the setters
+    $excel->setCreator('Maatwebsite')
+          ->setCompany('Maatwebsite');
+
+    // Call them separately
+    $excel->setDescription('A demonstration to change the file properties');
+
+    
+	 $excel->sheet('Sheetname', function($sheet) use($arr) {
+
+        $sheet->fromArray($arr);
+
+    });    
+
+
+	})->export('xls');
+});
